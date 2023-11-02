6195e0143d54995cdc5564e27395d92bbf4aa2bf
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-02 22:50:07 +0530

    weeks

---
+++ MonthStarter.java
@@ -0,0 +1,45 @@
+class MonthStarter{
+	public Static void main(String[] dayd){
+		
+		System.out.println("Starting main method");
+		
+		Month month=new Month("jan",31,4,6);
+		Month.displayInfo();
+		
+		Month month1=new Month("feb",28,4,4);
+		Month1.displayInfo();
+		
+		Month month2=new Month("march",30,4,7);
+		Month2.displayInfo();
+		
+		Month month3=new Month("april",31,4,5);
+		Month3.displayInfo();
+		
+		Month month4=new Month("may",30,4,4);
+		Month4.displayInfo();
+		
+		Month month5=new Month("june",31,4,5);
+		Month5.displayInfo();
+		
+		Month month6=new Month("july",30,4,6);
+		Month6.displayInfo();
+		
+		Month month7=new Month("aug",31,4,5);
+		Month7.displayInfo();
+		
+		Month month8=new Month("sept",30,4,7);
+		Month8.displayInfo();
+		
+		Month month9=new Month("oct",31,4,4);
+		Month9.displayInfo();
+		
+		Month month10=new Month("nov",30,4,6);
+		Month10.displayInfo();
+		
+		Month month11=new Month("Dec",31,4,4);
+		Month11.displayInfo();
+		
+		
+		
+	}
+}
