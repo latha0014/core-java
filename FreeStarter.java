ace4d07b9bee2a36abd48974b9b8e73a7c54f434
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-02 22:48:03 +0530

    what

---
+++ FreeStarter.java
@@ -0,0 +1,42 @@
+class FreeStarter{
+	public static void main(String[] isfree){
+		System.out.println("starting main method");
+		
+		
+		Free free=new Free("pizza","LuckyCoupen",false);
+		free.displayInfo();
+		
+		Free free1=new Free("Course","Campus",true);
+		free1.displayInfo();
+		
+		Free free2=new Free("cloths","FestiveOffer",true);
+		free2.displayInfo();
+		
+		Free free3=new Free("amount","ByGovt",false);
+		free3.displayInfo();
+		
+		Free free4=new Free("Wifi","toUse",false);
+		free4.displayInfo();
+		
+		Free free5=new Free("Gold","LuckyDraw",true);
+		free5.displayInfo();
+		
+		Free free6=new Free("Water","ToLive",false);
+		free6.displayInfo();
+		
+		Free free7=new Free("Air","ByNature",true);
+		free7.displayInfo();
+		
+		Free free8=new Free("moble","FestiveOffer",true);
+		free8.displayInfo();
+		
+		Free free9=new Free("food","LuckyDraw",true);
+		free9.displayInfo();
+		
+		Free free10=new Free("pizza","LuckyCoupen",false);
+		free10.displayInfo();
+		
+		Free free11=new Free("SunLight","ByNature",true);
+		free11.displayInfo();
+	}
+}
