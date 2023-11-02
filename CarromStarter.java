502786d6b392f009f88175f7e511ab8c8fbad700
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-02 10:21:28 +0530

    aksljk

---
+++ CarromStarter.java
@@ -0,0 +1,42 @@
+class CarromStarter{
+	public static void main(String[] args){
+		System.out.println("Starting Carrom in CarromStarter");
+		
+		
+		
+		Carrom Carrom=new Carrom();
+		System.out.println("Carrom coinsColor is:" +Carrom.coinsColor);
+		System.out.println("Carrom noOfCoins is :" +Carrom.noOfCoins);
+		System.out.println("Carrom type is :" +Carrom.type);
+		System.out.println("Carrom cost is :" +Carrom.cost);
+	
+	
+		Carrom Carrom1=new Carrom();
+		System.out.println("Carrom1 coinsColor is:" +Carrom1.coinsColor);
+		System.out.println("Carrom1 noOfCoins is :" +Carrom1.noOfCoins);
+		System.out.println("Carrom1 type is :" +Carrom1.type);
+		System.out.println("Carrom1 cost is :" +Carrom1.cost);
+		
+		Carrom Carrom2=new Carrom();
+		System.out.println("Carrom2 coinsColor is:" +Carrom2.coinsColor);
+		System.out.println("Carrom2 noOfCoins is :" +Carrom2.noOfCoins);
+		System.out.println("Carrom2 type is :" +Carrom2.type);
+		System.out.println("Carrom2 cost is :" +Carrom2.cost);
+		
+		Carrom Carrom3=new Carrom();
+		System.out.println("Carrom3 coinsColor is:" +Carrom3.coinsColor);
+		System.out.println("Carrom3 noOfCoins is :" +Carrom3.noOfCoins);
+		System.out.println("Carrom3 type is :" +Carrom3.type);
+		System.out.println("Carrom3 cost is :" +Carrom3.cost);
+		
+		Carrom Carrom4=new Carrom();
+		System.out.println("Carrom4 coinsColor is:" +Carrom4.coinsColor);
+		System.out.println("Carrom4 noOfCoins is :" +Carrom4.noOfCoins);
+		System.out.println("Carrom4 type is :" +Carrom4.type);
+		System.out.println("Carrom4 cost is :" +Carrom4.cost);
+		
+		System.out.println("Ending Carrom in CarromStarter");
+		
+	}
+}
+}
