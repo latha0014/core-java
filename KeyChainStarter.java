8f63d297c44f91950291584c77ffabc6e2da0e61
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-02 10:29:12 +0530

    gift

---
+++ KeyChainStarter.java
@@ -0,0 +1,45 @@
+class KeyChainStarter{
+	public static void main(String[] args){
+		System.out.println("Starting Keychain in KeyChainStarter");
+		
+		
+		
+		Keychain Keychain=new Keychain();
+		System.out.println("Keychain Color is:" +Keychain.coinsColor);
+		System.out.println("Keychain brand is :" +Keychain.brand);
+		System.out.println("Keychain type is :" +Keychain.type);
+		System.out.println("Keychain cost is :" +Keychain.cost);
+		
+		Keychain Keychain=new Keychain();
+		System.out.println("Keychain Color is:" +Keychain.coinsColor);
+		System.out.println("Keychain brand is :" +Keychain.brand);
+		System.out.println("Keychain type is :" +Keychain.type);
+		System.out.println("Keychain cost is :" +Keychain.cost);
+		
+		
+		Keychain Keychain=new Keychain();
+		
+		System.out.println("Keychain Color is:" +Keychain.coinsColor);
+		System.out.println("Keychain brand is :" +Keychain.brand);
+		System.out.println("Keychain type is :" +Keychain.type);
+		System.out.println("Keychain cost is :" +Keychain.cost);
+		
+		
+		Keychain Keychain=new Keychain();
+	
+		System.out.println("Keychain Color is:" +Keychain.coinsColor);
+		System.out.println("Keychain brand is :" +Keychain.brand);
+		System.out.println("Keychain type is :" +Keychain.type);
+		System.out.println("Keychain cost is :" +Keychain.cost);
+		
+		
+		Keychain Keychain=new Keychain();
+		System.out.println("Keychain Color is:" +Keychain.coinsColor);
+		System.out.println("Keychain brand is :" +Keychain.brand);
+		System.out.println("Keychain type is :" +Keychain.type);
+		System.out.println("Keychain cost is :" +Keychain.cost);
+		
+		
+		System.out.println("ending main method");
+	}
+}
