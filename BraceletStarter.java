5da6a39476f758e9e4bc9d2cbca4dfeeb7337071
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-02 10:15:28 +0530

    kk

---
+++ BraceletStarter.java
@@ -0,0 +1,40 @@
+class BraceletStarter {
+    public static void main(String[] args) {
+        System.out.println("Main method is started");
+
+        Bracelet Bracelet = new Bracelet();
+        System.out.println("size of Bracelet is: " + Bracelet.size);
+        System.out.println("Type of Bracelet is: " + Bracelet.Type);
+        System.out.println("cost of Bracelet is: " + Bracelet.cost);
+        System.out.println("shape of Bracelet is: " + Bracelet.shape);
+		
+		Bracelet Bracelet1 = new Bracelet();
+        System.out.println("size of Bracelet1 is: " + Bracelet1.size);
+        System.out.println("Type of Bracelet1 is: " + Bracelet1.Type);
+        System.out.println("cost of Bracelet1 is: " + Bracelet1.cost);
+        System.out.println("shape of Bracelet1 is: " + Bracelet1.shape);
+		
+		
+		Bracelet Bracelet 2= new Bracelet();
+        System.out.println("size of Bracelet2 is: " + Bracelet2.size);
+        System.out.println("Type of Bracelet2 is: " + Bracelet2.Type);
+        System.out.println("cost of Bracelet2 is: " + Bracelet2.cost);
+        System.out.println("shape of Bracelet2 is: " + Bracelet2.shape);
+		
+		Bracelet Bracelet = new Bracelet();
+        System.out.println("size of Bracelet3 is: " + Bracelet3.size);
+        System.out.println("Type of Bracelet3 is: " + Bracelet3.Type);
+        System.out.println("cost of Bracelet3 is: " + Bracelet3.cost);
+        System.out.println("shape of Bracelet3 is: " + Bracelet3.shape);
+		
+		Bracelet Bracelet4 = new Bracelet();
+        System.out.println("size of Bracelet4 is: " + Bracelet4.size);
+        System.out.println("Type of Bracelet4 is: " + Bracelet4.Type);
+        System.out.println("cost of Bracelet4 is: " + Bracelet4.cost);
+        System.out.println("shape of Bracelet4 is: " + Bracelet4.shape);
+		
+		System.out.println("ending main method");
+	}
+	
+}
+       
