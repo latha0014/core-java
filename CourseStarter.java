454b595c2d8cb3d57a9b14be004a5fafad2e75a1
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-02 22:49:01 +0530

    python

---
+++ CourseStarter.java
@@ -0,0 +1,52 @@
+class CourseStarter{
+	public static void main(String[] args){
+		System.out.println("Starting main method");
+		
+		Course course=new Course("xworkz","BTM 2nd Stage",24000,"java",5,"good","good");
+		Course.displayInfo();
+		
+		
+		Course course1=new Course("kodnest","BTM 1st Stage",28000,"python",5,"good","good");
+		Course1.displayInfo();
+		
+		Course course2=new Course("jspiders","BTM 2nd Stage",30000,"java",5,"good","good");
+		Course2.displayInfo();
+		
+		Course course3=new Course("qspiders","basavangudi",32000,"java",5,"good","good");
+		Course3.displayInfo();
+		
+		
+		Course course4=new Course("mainTech","JPnagar",25000,"java",5,"good","good");
+		Course4.displayInfo();
+		
+		
+		Course course5=new Course("homeTute","BTM 3rd stage",28000,"java",5,"good","good");
+		Course5.displayInfo();
+		
+		
+		Course course6=new Course("newTch","Indiranagar",24000,"java",5,"good","good");
+		Course6.displayInfo();
+		
+		
+		Course course7=new Course("beingLearner","yalahanka",38000,"java",5,"good","good");
+		Course7.displayInfo();
+		
+		
+		Course course8=new Course("realTimeExperts","ElectronicCity",40000,"java",5,"good","good");
+		Course8.displayInfo();
+		
+		
+		Course course9=new Course("infoCampus","ElectronicCity",38000,"java",5,"good","good");
+		Course9.displayInfo();
+		
+		Course course10=new Course("achiversIT","BTM 2nd Stage",26000,"java",5,"good","good");
+		Course10.displayInfo();
+		
+		Course course11=new Course("xworkz","BTM 2nd Stage",24000,"java",5,"good","good");
+		Course11.displayInfo();
+		
+		
+		
+		
+	}
+}
