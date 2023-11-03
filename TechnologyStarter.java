d4903a6682848f270d022b54aef68f387668d6c7
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-03 09:30:40 +0530

    java

---
+++ TechnologyStarter.java
@@ -0,0 +1,43 @@
+class TechnologyStarter{
+	public static void main(String[] tech){
+		
+		System.out.println("Starting main method");
+		
+		Technology technology=new Technology();
+		Technology.displayInfo();
+		
+		Technology technology1=new Technology();
+		Technology1.displayInfo();
+		
+		Technology technology2=new Technology();
+		Technology2.displayInfo();
+		
+		Technology technology3=new Technology();
+		Technology3.displayInfo();
+		
+		Technology technology4=new Technology();
+		Technology4.displayInfo();
+		
+		Technology technology5=new Technology();
+		Technology5.displayInfo();
+		
+		Technology technology6=new Technology();
+		Technology6.displayInfo();
+		
+		Technology technology7=new Technology();
+		Technology7.displayInfo();
+		
+		Technology technology8=new Technology();
+		Technology8.displayInfo();
+		
+		Technology technology9=new Technology();
+		Technology9.displayInfo();
+		
+		Technology technology10=new Technology();
+		Technology10.displayInfo();
+		
+		Technology technology11=new Technology();
+		Technology11.displayInfo();
+		
+	}
+}
