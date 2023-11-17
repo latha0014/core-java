fac86ae25129825707854c4a6de78086f4ce531e
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-17 10:45:29 +0530

    menu

---
+++ FoodStarter.java
@@ -0,0 +1,54 @@
+class FoodStarter{
+	public static void main(String[] Food){
+		System.out.println("Starting main in FoodStarter");
+		
+		Food food=new food();
+		Food food1=new Sweet();
+		Food food2=new Ladoo();
+		Food food3=new MotichurLadoo();
+		Food food4=new Biscuit();
+		Food food5=new Tiger();
+		Food food6=new SpecialTiger();
+		Food food7=new GlucoTiger();
+		Food food8=new Parle();
+		Food food9=new GlucoParle();
+		
+		
+		Sweet sweet=new Sweet();
+		Sweet sweet1=new Ladoo();
+		Sweet sweet2=new MotichurLadoo();
+		
+		
+		Ladoo ladoo=new Ladoo();
+		Ladoo ladoo1=new MotichurLadoo();
+		
+		
+		MotichurLadoo motichurLadoo=new MotichurLadoo();
+		System.out.println("===========================================");
+		
+		
+		Biscuit biscuit=new Biscuit();
+		Biscuit biscuit1=new Tiger();
+		Biscuit biscuit2=new SpecialTiger();
+		Biscuit biscuit3=new GlucoTiger();
+		Biscuit biscuit4=new Parle();
+		Biscuit biscuit5=new GlucoParle();
+		
+		
+		Tiger tiger=new Tiger();
+		Tiger tiger1=new SpecialTiger();
+		Tiger tiger2=new GlucoTiger();
+		
+		SpecialTiger specialTiger=new SpecialTiger();
+		
+		Parle parle=new Parle();
+		Parle parle1=new GlucoParle();
+		
+		GlucoParle glucoParle=new GlucoParle();
+		
+		System.out.println("ending main");	
+		
+		
+		
+	}
+}
