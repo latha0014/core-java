ecaf0b1ea5bac3df09ce646f09939aaa3aa91b6a
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:25:00 +0530

    testing

---
+++ Tester.java
@@ -0,0 +1,14 @@
+class Tester extends Developer{
+	
+	Tester()
+	{
+		System.out.println("this is default constructor with no-argument of Tester");
+	}
+	
+	void qualityCheck()
+	{
+		System.out.println("tester has qualityCheck method");
+	}
+	
+	
+}
