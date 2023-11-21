1518d7ac00c729696ba552a22647c2870ba41f17
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:52:28 +0530

    coding

---
+++ Developer.java
@@ -0,0 +1,12 @@
+class Developer extends Person{
+	
+	Developer()
+	{
+		System.out.println("this is default constructor with no-argument of Developer");
+	}
+	
+	void code()
+	{
+		System.out.println("Developer has code method");
+	}
+}
