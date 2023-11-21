d6b54fe36f7aa2ba87d4c26c0d44be2daa5fef2b
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:26:00 +0530

    PD

---
+++ ProductOwner.java
@@ -0,0 +1,14 @@
+class ProductOwner extends Person{
+	
+	ProductOwner()
+	{
+		System.out.println("this is default constructor with no-argument of ProductOwner");
+	}
+	
+	void validate()
+	{
+		System.out.println("ProductOwner is validate method");
+	}
+	
+	
+}
