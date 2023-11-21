eac2ff538e6ed67632c06e4e73f7be816068054f
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:51:45 +0530

    sub

---
+++ Devops.java
@@ -0,0 +1,13 @@
+class Devops extends Person{
+	
+	Devops(){
+			System.out.println("this is default constructor with no-argument of Devops");
+	}
+	
+	void runBuild()
+	{
+		System.out.println("Devops is runBuild method");
+
+	}
+	
+}
