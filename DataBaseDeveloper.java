aa1c158d0235b188cede7b424c97f828eeea62eb
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:53:35 +0530

    data

---
+++ DataBaseDeveloper.java
@@ -0,0 +1,14 @@
+class DataBaseDeveloper extends Developer{
+	
+	DataBaseDeveloper()
+	{
+		System.out.println("this is default constructor with no-argument of BusinessAnalyst");
+	}
+	
+	void runScripts()
+	{
+		System.out.println("DataBaseDeveloper is runScripts method");
+
+	}
+	
+}
