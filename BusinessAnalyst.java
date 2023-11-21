22db58e21b31888d5c47cc954a84cab97314edbb
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:54:05 +0530

    Analyst

---
+++ BusinessAnalyst.java
@@ -0,0 +1,16 @@
+class BusinessAnalyst extends Person{
+	
+	BusinessAnalyst()
+	{
+		System.out.println("this is default constructor with no-argument of BusinessAnalyst");
+	}
+
+		void checkRequirement()
+		{
+			System.out.println("BusinessAnalyst has checkRequirement method");
+
+		}
+
+
+
+}
