190cb0169379a91a01e88f3aaac5528d5b67c567
latha0014 <hemalathab.xworkz@gmail.com>
2023-11-21 11:51:15 +0530

    inheritence

---
+++ Person.java
@@ -0,0 +1,18 @@
+class Person{
+	
+	Person()
+	{
+		System.out.println("this is default constructor with no-argument of Person");
+	}
+	
+	void eat()
+	{
+      System.out.println("person has eat method");
+	}
+	void Sleep()
+	{
+		   System.out.println("person has Sleep method");
+
+	}
+	
+}
